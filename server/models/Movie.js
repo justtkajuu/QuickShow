74c83040730d414f0826232fcf5bea9b42cda489
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-26 15:05:22 +0530

    fix: correct model name typo in Movie schema and update getShows function to filter upcoming shows

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -18,6 +18,6 @@ const movieSchema = new mongoose.Schema(
     {timestamps:true}
 )
 
-const Movie = mongoose.model('Moive', movieSchema)
+const Movie = mongoose.model('Movie', movieSchema)
 
 export default Movie
