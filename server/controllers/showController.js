9d3785d76ee7f9ea2de9569da87aebfdf9e02499
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-26 15:14:53 +0530

    fix: update getShows function to remove date filter and sort shows by showDateTime

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -97,7 +97,7 @@ export const addShow = async (req, res) => {
 
 export const getShows = async (req, res) => {
   try {
-    const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({showDateTime : 1})
+    const shows = await Show.find({}).populate('movie')
 
     // filter unique shows
     const uniqueshows = new Set(shows.map((show) => show.movie));
