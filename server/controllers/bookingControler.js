1bf8e2ad6e678ce03a1296ec99b77218f0fd0a3f
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-28 21:23:13 +0530

    fix: correct typo in getOccupiedSeats function reference in booking controller and routes

--- server/controllers/bookingControler.js
+++ server/controllers/bookingControler.js
@@ -70,7 +70,7 @@ export const createBooking = async (req, res ) => {
     }
 }
 
-export const getOcuupiedSeats = async (req, res) => {
+export const getOccupiedSeats = async (req, res) => {
     try{
        const {showId} = req.params
        const showData = await Show.findById(showId)
--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createBooking, getOcuupiedSeats } from '../controllers/bookingControler.js';
+import { createBooking, getOccupiedSeats,  } from '../controllers/bookingControler.js';
 
 const bookingRouter = express.Router()
 
 bookingRouter.post('/create', createBooking)
-bookingRouter.get('/seats/:showId', getOcuupiedSeats)
+bookingRouter.get('/seats/:showId', getOccupiedSeats)
 
 export default bookingRouter
 
