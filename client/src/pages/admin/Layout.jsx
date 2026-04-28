7a300c1b7056285078de72151d63dcab38db5e0b
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-28 11:30:07 +0530

    fix: integrate useAppContext for admin state management in Layout component

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -2,10 +2,20 @@
 import AdminNavabar from '../../components/admin/AdminNavabar'
 import AdminSidebar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
+import { useAppContext } from '../../context/AppContext'
+import { useEffect } from 'react'
+import Loading from '../../components/Loading'
 
 
 const Layout = () => {
-  return (
+
+  const {isAdmin, fetchIsAdmin} = useAppContext()
+
+  useEffect(() => {
+    fetchIsAdmin()
+  },[])
+
+  return isAdmin ? (
     <>
       <AdminNavabar/>
       <div className='flex'>
@@ -15,6 +25,8 @@ const Layout = () => {
         </div>
       </div>
     </>
+  ) : (
+    <Loading/>
   )
 }
 
