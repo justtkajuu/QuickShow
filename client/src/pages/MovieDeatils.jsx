85f3972100eb1ff8b6c21337bee14aefebdc1fbd
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-29 20:50:45 +0530

    fix: clean up formatting and improve readability in MovieDeatils component

--- client/src/pages/MovieDeatils.jsx
+++ client/src/pages/MovieDeatils.jsx
@@ -1,139 +1,176 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
-import BlurCircle from '../components/BlurCircle'
-import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
-import TimeFormat from '../lib/TimeFormat'
-import DateSelect from '../components/DateSelect'
-import MovieCard from '../components/MovieCard'
-import Loading from '../components/Loading'
-import { useAppContext } from '../context/AppContext'
-import toast from 'react-hot-toast'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
+import BlurCircle from "../components/BlurCircle";
+import { Heart, PlayCircleIcon, StarIcon } from "lucide-react";
+import TimeFormat from "../lib/TimeFormat";
+import DateSelect from "../components/DateSelect";
+import MovieCard from "../components/MovieCard";
+import Loading from "../components/Loading";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 const MovieDeatils = () => {
-  const {id} = useParams()
-  const [show, setShow] = useState(null)
-  const navigate = useNavigate()
-  const {shows, axios, getToken, user, favorites,fetchFavoriteMovie,
-  image_base_url} = useAppContext()
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+  const navigate = useNavigate();
+  const {
+    shows,
+    axios,
+    getToken,
+    user,
+    favorites,
+    fetchFavoriteMovie,
+    image_base_url,
+  } = useAppContext();
 
-  const getShow = async ()=>{
+  const getShow = async () => {
     try {
       const { data } = await axios.get(`/api/show/${id}`);
 
-      if(data.success){
-        setShow(data)
+      if (data.success) {
+        setShow(data);
       }
-
     } catch (error) {
       console.error(error);
-      
     }
-  }
+  };
 
   const handleFavorite = async () => {
-  try {
-    if (!user) return toast.error("Please login to proceed.");
+    try {
+      if (!user) return toast.error("Please login to proceed.");
 
-    const { data } = await axios.post(
-      "/api/user/update-favorite",
-      { movieId: id },
-      {
-        headers: {
-          Authorization: `Bearer ${await getToken()}`,
+      const { data } = await axios.post(
+        "/api/user/update-favorite",
+        { movieId: id },
+        {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+          },
         },
+      );
+
+      if (data.success) {
+        await fetchFavoriteMovie();
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
       }
-    );
-
-    if (data.success) {
-      await fetchFavoriteMovie();
-      toast.success(data.message);
-    } else {
-      toast.error(data.message);
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
     }
-  } catch (error) {
-    console.error(error);
-    toast.error(error.message);
-  }
-};
+  };
 
-  useEffect(()=>{
-    getShow()
-  },[id])
+  useEffect(() => {
+    getShow();
+  }, [id]);
 
   return show ? (
-    <div className='ox-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
-     
-      <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
-        
-        <img src={image_base_url + show.movie.poster_path} alt="" className='max-md:mx-auto rounded-xl h-104 max-w-70 object-cover' />
+    <div className="ox-6 md:px-16 lg:px-40 pt-30 md:pt-50">
+      <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
+        <img
+          src={image_base_url + show.movie.poster_path}
+          alt=""
+          className="max-md:mx-auto rounded-xl h-104 max-w-70 object-cover"
+        />
 
-        <div className='relative flex flex-col gap-3'>
-       
-        <BlurCircle top='-100px' left='-100px'/>
+        <div className="relative flex flex-col gap-3">
+          <BlurCircle top="-100px" left="-100px" />
 
-        <p className='text-primary'>ENGLISH</p>
+          <p className="text-primary">ENGLISH</p>
 
-        <h1 className='text-4xl font-semibold max-w-96 text-balance'>{show.movie.title}</h1>
+          <h1 className="text-4xl font-semibold max-w-96 text-balance">
+            {show.movie.title}
+          </h1>
 
-        <div className='flex items-center gap-2 text-gray-300'>
-          <StarIcon className='w-5 h-5 text-primary fill-primary'/>
-          {show.movie.vote_average.toFixed(1)} User Rating
-        </div>
+          <div className="flex items-center gap-2 text-gray-300">
+            <StarIcon className="w-5 h-5 text-primary fill-primary" />
+            {show.movie.vote_average.toFixed(1)} User Rating
+          </div>
 
-        <p className='text-gray-400 mt-2 text-sm leading-tight max-w-xl'>{show.movie.overview}</p>
+          <p className="text-gray-400 mt-2 text-sm leading-tight max-w-xl">
+            {show.movie.overview}
+          </p>
 
-        <p>{TimeFormat(show.movie.runtime)} . {show.movie.genres.map(genre => genre.name).join(", ")} . {show.movie.release_date.split("-")[0]}</p>
+          <p>
+            {TimeFormat(show.movie.runtime)} .{" "}
+            {show.movie.genres.map((genre) => genre.name).join(", ")} .{" "}
+            {show.movie.release_date.split("-")[0]}
+          </p>
 
-        <div className='flex items-center flex-wrap gap-4 mt-4'>
+          <div className="flex items-center flex-wrap gap-4 mt-4">
+            <button className="flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95">
+              <PlayCircleIcon className={`w-5 h-5`} />
+              Watch Trailer
+            </button>
 
-          <button className='flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95'>
-            <PlayCircleIcon className={`w-5 h-5`}/>
-            Watch Trailer
-          </button>
+            <a
+              className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95"
+              href="#dateSelect"
+            >
+              Buy Tickets
+            </a>
 
-          <a className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95' href="#dateSelect">Buy Tickets
-          </a>
-
-          <button onClick={handleFavorite} className='bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95'>
-            <Heart className={`w-5 h-5 ${favorites?.find(movie => movie._id === id) ? "fill-primary text-white" : ""}`}/>
-          </button>
-
-        </div>
+            <button
+              onClick={handleFavorite}
+              className="bg-gray-700 p-2.5 rounded-full transition cursor-pointer active:scale-95"
+            >
+              <Heart
+                className={`w-5 h-5 ${favorites?.find((movie) => movie._id === id) ? "fill-primary text-white" : ""}`}
+              />
+            </button>
+          </div>
         </div>
       </div>
 
+      <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
 
-      <p className='text-lg font-medium mt-20'>Your Favorite Cast</p>
-
-      <div className='overflow-x-auto no-scrollbar mt-8 pb-4'>
-        <div className='flex items-center gap-4 w-max px-4'>
-          {show.movie.casts.slice(0,12).map((cast,index)=>(
-            <div key={index} className='flex flex-col items-center text-center'>
-              <img src={image_base_url + cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
+      <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+        <div className="flex items-center gap-4 w-max px-4">
+          {show.movie.casts.slice(0, 12).map((cast, index) => (
+            <div key={index} className="flex flex-col items-center text-center">
+              <img
+                src={
+                  cast.profile_path
+                    ? image_base_url + cast.profile_path
+                    : "/profile.png"
+                }
+                alt={cast.name}
+                className="rounded-full h-20 md:h-20 aspect-square object-cover bg-gray-800"
+              />
               <p>{cast.name}</p>
             </div>
           ))}
         </div>
       </div>
 
-      <DateSelect dateTime={show.dateTime} id={id}/>
+      <DateSelect dateTime={show.dateTime} id={id} />
 
-      <p className='text-lg font-medium mt-20 mb-8'>You may Also Like</p>
-      <div className='flex flex-wrap max-sm:justify-center justify-between gap-8'>
-        {shows.slice(0,4).map((movie,index)=>(
-          <MovieCard key={index} movie={movie}/>
+      <p className="text-lg font-medium mt-20 mb-8">You may Also Like</p>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {shows.slice(0, 4).map((movie, index) => (
+          <MovieCard key={index} movie={movie} />
         ))}
       </div>
 
-      <div className='flex justify-center mt-20'>
-        <button onClick={()=>{navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show More</button>
+      <div className="flex justify-center mt-20">
+        <button
+          onClick={() => {
+            navigate("/movies");
+            scrollTo(0, 0);
+          }}
+          className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
+        >
+          Show More
+        </button>
       </div>
-
     </div>
-  ) : <div>
-    <Loading/>
-  </div>
-}
+  ) : (
+    <div>
+      <Loading />
+    </div>
+  );
+};
 
-export default MovieDeatils
+export default MovieDeatils;
