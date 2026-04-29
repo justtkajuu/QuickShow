c8ac6092aebb71c7503c7411d2ec09e0636c8717
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-29 13:44:49 +0530

    fix: correct nodemailer transport method from createTestAccount to createTransport

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-const transporter = nodemailer.createTestAccount({
+const transporter = nodemailer.createTransport({
     host:'smtp-relay.brevo.com',
     port: 587,
     auth: {
