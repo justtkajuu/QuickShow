f49b57710525e898f85564833202245f1c3ae9a0
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-30 10:55:18 +0530

    refactor: improve BlurCircle component by enhancing styling and simplifying props usage

--- client/src/components/BlurCircle.jsx
+++ client/src/components/BlurCircle.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 
-const BlurCircle = ({top = "auto", left= "auto", right="auto", bottom="auto" }) => {
+const BlurCircle = ({ top = "auto", left = "auto", right = "auto", bottom = "auto" }) => {
   return (
-    <div className='absolute -z-50 h-58 w-58 aspect-square rounded-full bg-primary/30 blur-3xl' style={{
-        top:top,
-        left:left,
-        right:right,
-        bottom:bottom
-    }}>
-      
-    </div>
-  )
-}
+    <div
+      className="absolute -z-50 h-58 w-58 aspect-square rounded-full bg-gradient-to-r from-blue-500/30 to-purple-500/30 blur-3xl"
+      style={{ top, left, right, bottom }}
+    />
+  );
+};
 
 export default BlurCircle
