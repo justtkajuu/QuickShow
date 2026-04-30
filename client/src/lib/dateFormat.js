0b98c1d7e9ee357da480de4fcb31688fdf724ce2
justtkajuu <kajalgolghate2004@gmail.com>
2026-04-30 21:58:09 +0530

    fix: Enhance date formatting in dateFormat function to include hour12 and timeZone options for improved display

--- client/src/lib/dateFormat.js
+++ client/src/lib/dateFormat.js
@@ -1,9 +1,13 @@
 export const dateFormat = (date) => {
-    return new Date(date).toLocaleString("en-US", {
-        weekday: "short",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-    });
-}
+  const d = new Date(date);
+
+  return d.toLocaleString("en-US", {
+    weekday: "short",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+    timeZone: "Asia/Kolkata",
+  });
+};
